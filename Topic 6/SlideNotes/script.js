9ed2c7856729b017f89b02cd055d512ef0ec864e
Safia07 <safia.bi.07@gmail.com>
2022-04-25 17:56:35 +0100

    Topic 6, Ex1 code

--- Topic 6/SlideNotes/script.js
+++ Topic 6/SlideNotes/script.js
@@ -10,6 +10,7 @@ function myLoadFunction() {
     // The two lines of code above will run immediately if they weren't in the function. 
 }
 
+document.addEventListener('DOMContentLoaded', scriptLoaded);
 document.addEventListener('DOMContentLoaded', myLoadFunction);
 // addEventListener = It runs a function when a specific event occurs. In the brackets, whatever is written in these speech marks '' tells the programme when it happens and whatever is written after the comma is the name of the function that will run. 
 // DOMContentLoaded = the elements (tags in the HTML tag e.g. p tag) exists when the content of the page has loaded. 
