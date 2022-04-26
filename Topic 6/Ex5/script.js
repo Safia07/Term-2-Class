2bf7dd09c22bbf504ecc37d4111a3d49c887694e
Safia07 <safia.bi.07@gmail.com>
2022-04-26 18:12:50 +0100

    Topic 6 Ex5 created, copied text from Ex4

---
+++ Topic 6/Ex5/script.js
@@ -0,0 +1,9 @@
+function scriptLoaded() {
+    var element = document.getElementById('H1');
+    element.firstChild.nodeValue = 'New Heading';
+
+    var element = document.getElementById('P1');
+    element.firstChild.nodeValue = 'New Content on February 30th 2029';
+}
+
+document.addEventListener('DOMContentLoaded', scriptLoaded);
