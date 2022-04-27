d2ccbb1e6b6b60287bf2a1b8428e125525bcaddb
Safia07 <safia.bi.07@gmail.com>
2022-04-27 12:01:55 +0100

    Topic 6 Ex7 completed

--- Topic 6/Ex7/script.js
+++ Topic 6/Ex7/script.js
@@ -1,5 +1,13 @@
-function myinputLoaded() {
+function clickFunction() {
     var element = document.getElementById('myinput');
-        alert(element.value);
+
+    var div = document.getElementById('myresult');
+    div.firstChild.nodeValue = element.value;
 }
 
+function loadFunction() {
+    var element = document.getElementById('myinput');
+    element.addEventListener('keyup', clickFunction);
+}
+
+document.addEventListener('DOMCintentLoaded', loadFunction);
