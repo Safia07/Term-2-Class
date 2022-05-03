0016b12eadb38eebb49d281ad597ccf9bfd6bd69
Safia07 <safia.bi.07@gmail.com>
2022-05-03 18:27:09 +0100

    added page no to ex1 js, T7-1 Ex2 completed

--- Topic 7-1/Ex2/script.js
+++ Topic 7-1/Ex2/script.js
@@ -1,4 +1,4 @@
-// Page 20
+// page 26
 
 function loadFunction() {
     var element = document.getElementById('circle');
@@ -7,6 +7,8 @@ function loadFunction() {
 
 function clickCircle() {
     alert('The button was pressed');
+    var circle = document.getElementById('circle');
+    circle.style.backgroundColor = 'blue';
 }
 
 document.addEventListener('DOMContentLoaded', loadFunction);
