7702b871d8d761a19c77b58b9a286708f6939347
Safia07 <safia.bi.07@gmail.com>
2022-05-03 17:20:31 +0100

    Topic 7 Ex1 completed

--- Topic 7-1/Ex1/script.js
+++ Topic 7-1/Ex1/script.js
@@ -1,1 +1,10 @@
-document.addEventListener('DOMContentLoaded');
+function loadFunction() {
+    var element = document.getElementById('circle');
+    circle.addEventListener('click', clickCircle);
+}
+
+function clickCircle() {
+    alert('The button was pressed');
+}
+
+document.addEventListener('DOMContentLoaded', loadFunction);
