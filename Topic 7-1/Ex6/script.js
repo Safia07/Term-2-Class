6d779b42443ed9d020ce7dc9153b1f5ef7f3c867
Safia07 <safia.bi.07@gmail.com>
2022-05-05 13:31:53 +0100

    T7-1 Ex6 created

---
+++ Topic 7-1/Ex6/script.js
@@ -0,0 +1,16 @@
+// page 37
+
+function loadFunction() {
+    var element = document.getElementById('circle');
+    circle.addEventListener('click', clickCircle);
+    element.style.opacity = '1';
+
+}
+
+function clickCircle() {
+    var element = document.getElementById('circle');
+    var circleOpacity = parseFloat(element.style.opacity);
+    element.style.opacity = circleOpacity - 0.1;
+}
+
+document.addEventListener('DOMContentLoaded', loadFunction);
